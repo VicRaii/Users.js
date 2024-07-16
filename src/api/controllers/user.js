54a31a5b38b46d0db56562a3b9f9d782c7b8f115
vimarupon@hotmail.com <vimarupon@gmail.com>
2024-07-16 16:25:08 +0200

    directo acabado

--- src/api/controllers/user.js
+++ src/api/controllers/user.js
@@ -1,4 +1,6 @@
+const { generateSign } = require("../../config/jwt");
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 const register = async (req, res, next) => {
   try {
@@ -21,4 +23,44 @@ const register = async (req, res, next) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ userName: req.body.userName });
+
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        //! Lo que pasa cuando te logueas con jsonwebtoken.
+        const token = generateSign(user._id);
+        return res.status(200).json({ user, token });
+      }
+      return res.status(400).json("El usuario o la contraseña son incorrectos");
+    } else {
+      return res.status(400).json("El usuario o la contraseña son incorrectos");
+    }
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const userDeleted = await User.findByIdAndDelete(id);
+    return res
+      .status(200)
+      .json({ mensaje: "Este usuario ha sido eliminado", userDeleted });
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
+
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
+
+module.exports = { register, login, deleteUser, getUsers };
--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -1,7 +1,16 @@
-const { register } = require("../controllers/user");
+const { isAuth, isPaquito } = require("../../middlewares/auth");
+const {
+  register,
+  login,
+  deleteUser,
+  getUsers,
+} = require("../controllers/user");
 
 const userRoutes = require("express").Router();
 
+userRoutes.get("/", [isAuth], getUsers);
 userRoutes.post("/register", register);
+userRoutes.post("/login", login);
+userRoutes.delete("/:id", [isPaquito], deleteUser);
 
 module.exports = userRoutes;
